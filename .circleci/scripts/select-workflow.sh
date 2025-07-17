d55d24570af6a43803ad075df5d8ae2ba68fc17d
Mateusz Hołowieszko <holowieszko.m@gmail.com>
2025-07-17 20:38:15 +0200

    export WORKFLOW_FILE env

--- .circleci/scripts/select-workflow.sh
+++ .circleci/scripts/select-workflow.sh
@@ -12,19 +12,12 @@ elif [[ "$BRANCH_NAME" == "develop" ]]; then
     WORKFLOW_FILE=".circleci/workflows/dev.yml"
 elif [[ "$BRANCH_NAME" =~ ^feat\/(.+)$ ]]; then
     ENV_NAME="${BASH_REMATCH[1]}"
+
     echo "Extracted environment: $ENV_NAME"
 
-    # Optional: dynamically set an env var or pass ENV_NAME to the workflow
-    export ENV_NAME
-
-    WORKFLOW_FILE=".circleci/workflows/dev.yml"
+    echo 'export WORKFLOW_FILE=.circleci/workflows/dev.yml' >>$BASH_ENV
+    echo "export ENV_NAME=$ENV_NAME" >>$BASH_ENV
 else
     echo "No workflow defined for branch: $BRANCH_NAME"
     exit 0
 fi
-
-echo "Using workflow file: $WORKFLOW_FILE"
-
-circleci continuation continue \
-    --config "$WORKFLOW_FILE" \
-    --revision "$CIRCLE_SHA1"
